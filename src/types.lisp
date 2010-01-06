6a0bcbb55f9ad18fdf19e14357de47975170ec0a
tpapp <tkpapp@gmail.com>
2010-01-06 19:16:52 +0100

    Cleanup of types code

--- src/types.lisp
+++ src/types.lisp
@@ -1,63 +1,52 @@
 (in-package :lla)
 
-;;;; ** Element types
-;;;;
-;;;; Some types are symbols (eg 'DOUBLE-FLOAT), some are lists (eg
-;;;; '(COMPLEX SINGLE-FLOAT).  CFFI uses its own naming scheme (and
-;;;; does not have complex types, as of Sep 2009), so we will
-;;;; introduce our own names -- which are sometimes appended to class
-;;;; and or function names --- and mappings, to and from the other
-;;;; naming schemes (currently only CL, as CFFI stuff is handled
-;;;; inside mem-aref*).
+;;;; Type for dimension.
 
-;;;; NOTE Types are hardwired, because I don't think I will need more.
-;;;; This is not as nice/robust as LISP-MATRIX, but lookup tables
-;;;; would not help me much, as (1) sometimes I need to handle special
-;;;; cases, eg complex types with mem-aref* and (2) I would need to
-;;;; define a very complex DSL for possible coercions. -- Tamas
+(deftype dimension ()
+   "Type for vector/matrix dimensions, basically a nonnegative fixnum."
+  '(integer 0 #.most-positive-fixnum))
+
+(defun check-index (index dimension)
+  "Error if index is outside dimension."
+  ;; ?? should this be a macro, could this signal more information
+  (unless (and (<= 0 index) (< index dimension))
+    (error "index ~a is outside [0,~a)" index dimension)))
+
+
+;;;; Element types
+;;;
+;;; Some types are symbols (eg 'DOUBLE-FLOAT), some are lists (eg
+;;; '(COMPLEX SINGLE-FLOAT).  CFFI uses its own naming scheme (and
+;;; does not have complex types, as of Sep 2009), so we will introduce
+;;; our own names -- which are sometimes appended to class and or
+;;; function names --- and mappings, to and from the other naming
+;;; schemes (currently only CL, as CFFI stuff is handled inside
+;;; mem-aref*).
+
+;;; NOTE Types are hardwired, because I don't think I will need more.
+;;; This is not as nice/robust as LISP-MATRIX, but lookup tables
+;;; would not help me much, as (1) sometimes I need to handle special
+;;; cases, eg complex types with mem-aref* and (2) I would need to
+;;; define a very complex DSL for possible coercions. -- Tamas
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (define-symbol-macro lla-types-list 
-      ;;  Return a list of LLA types.  For internal use only, NOT EXPORTED."
-      '(:single :double :complex-single :complex-double :integer)))
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
+  (define-symbol-macro lla-types-list   ; NOT EXPORTED
+      '(:single :double :complex-single :complex-double :integer))
   (deftype lla-type ()
     "All LLA types."
-    `(member ,@'#.lla-types-list))
-
-  ;; ?? maybe don't need to define the types below, -p functions should
-  ;; be enough -- Tamas
-  
-  (deftype lla-complex-type ()
-    "All LLA complex (float) types."
-    '(member :complex-single :complex-double))
-  
+    `(member ,@lla-types-list))
   (defun lla-complex-p (lla-type)
     "Non-nil iff complex type."
-    (check-type lla-type lla-type)
-    (typep lla-type 'lla-complex-type))
-  
-  (deftype lla-double-type ()
-    "All LLA double float types (real and complex)."
-    '(member :double :complex-double))
-  
+    (or (eq lla-type :complex-single) (eq lla-type :complex-double)))
   (defun lla-double-p (lla-type)
     "Non-nil iff double-precision type."
-    (check-type lla-type lla-type)
-    (typep lla-type 'lla-double-type)))
-
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
+    (or (eq lla-type :double) (eq lla-type :complex-double)))
   (defun coercible-p (lla-source-type lla-target-type)
-    "Permitted coercions for LLA types.  It is guaranteed that CL:COERCE
-can perform these coercions on the corresponding lisp types.
-
-Basic summary:
-
-- integers can be coerced to anything
-- single<->double precision coercions are possible both ways
-- real floats can be upgraded to complex."
+    "Permitted coercions for LLA types.  It is guaranteed that
+CL:COERCE can perform these coercions on the corresponding lisp
+types. Basic summary: (1) integers can be coerced to anything, \(2)
+single<->double precision coercions are possible both ways, (3) real
+floats can be upgraded to complex."
     (check-type lla-source-type lla-type)
     (check-type lla-target-type lla-type)
     (cond
@@ -71,7 +60,6 @@ Basic summary:
        nil)
       ;; all the rest should be possible
       (t t)))
-  
   (defun coercible-pairs-list ()
       ;;   "Generate the list of all LLA (source target) pairs for which
       ;; coercible-p holds.  For internal use only, NOT EXPORTED."
@@ -106,16 +94,9 @@ Basic summary:
     ;; !! should define & use conditions -- Tamas
     (otherwise (error "~a is not a valid LLA type" lla-type))))
 
-
-(defmacro for-coercible-pairs ((source target) form)
-  "Replace source and target with corresponding coercible pairs
-in form for each coercible pair in turn, wrap the result in a progn."
-  (cons 'progn
-	(mapcar (lambda (pair)
-		  (sublis (list (cons source (first pair))
-				(cons target (second pair)))
-			  form))
-		(coercible-pairs-list))))
+(defun coerce* (value lla-type)
+  "Coerce VALUE to type given by LLA-TYPE."
+  (coerce value (lla-type->lisp-type lla-type)))
 
 (defun smallest-common-target-type (lla-type-list)
   "Find the smallest supertype that all types can be coerced to.  Note
@@ -129,26 +110,18 @@ that this is always a float type because of LAPACK."
       (t :single))))
 
 (defun lla-type-classifier (sequence)
-  "Finds the smallest lla type that can accomodate the elements of
-  sequence.  If no such lla-type can be found, return nil."
-  (let ((typecode (reduce (lambda (x y)
-                            ;; parallel maximum on a cons
-                            (cons (max (car x) (car y)) (max (cdr x) (cdr y))))
-                          sequence
-                          :key (lambda (x)
-                                 ;; conses: precision (0: integer, 1:
-                                 ;; single, 2: double) and complex indicator
-                                 (cond
-                                   ((typep x '(signed-byte 32)) '(0 . 0))
-                                   ((typep x '(or single-float rational)) '(1 . 0))
-                                   ((typep x 'double-float) '(2 . 0))
-                                   ((typep x '(complex single-float)) '(1 . 1))
-                                   ((typep x '(complex double-float)) '(2 . 1))
-                                   (t (return-from lla-type-classifier nil)))))))
-    (cond
-      ((equal typecode '(0 . 0)) :integer)
-      ((equal typecode '(1 . 0)) :single)
-      ((equal typecode '(2 . 0)) :double) 
-      ((equal typecode '(1 . 1)) :complex-single)
-      ((equal typecode '(2 . 1)) :complex-double)
-      (t (error "internal error, this should never happen")))))
+  "Finds the smallest LLA-TYPE that can accomodate the elements of
+  sequence.  If no such LLA-TYPE can be found, return nil."
+  (ecase (reduce #'logior sequence :key
+                 (lambda (x) (typecase x ;; bits: float-p, double-p, complex-p
+                               ((signed-byte 32) #b000)
+                               ((or single-float rational) #b100)
+                               (double-float #b110)
+                               ((complex single-float) #b101)
+                               ((complex double-float) #b111)
+                               (t (return-from lla-type-classifier nil)))))
+    (#b000 :integer)
+    (#b100 :single)
+    (#b110 :double) 
+    (#b101 :complex-single)
+    (#b111 :complex-double)))     ; anything else should be impossible
