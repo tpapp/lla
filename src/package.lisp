f1138613a35e2ec73367475a10d5a257ca6a3b82
tpapp <tkpapp@gmail.com>
2012-01-10 19:20:12 +0100

    Reformatted defpackage to use uninterned symbols.

--- src/package.lisp
+++ src/package.lisp
@@ -1,56 +1,84 @@
 (defpackage #:lla
-  (:use common-lisp iterate let-plus cffi anaphora alexandria cl-num-utils)
-  (:shadowing-import-from cl-num-utils mean variance median)
+  (:use #:common-lisp #:iterate #:let-plus #:cffi #:anaphora #:alexandria
+        #:cl-num-utils)
+  (:shadowing-import-from #:cl-num-utils #:mean #:variance #:median)
   (:export 
-
-;;;; package-init - no exports
-
-;;;; basics
-
-;;; types
-
-   lla-integer lla-single lla-double lla-complex-single lla-complex-double
-
-;;; conditions
-   
-   lla-internal-error lla-unhandled-type lapack-error lapack-invalid-argument
-   lapack-failure lapack-singular-matrix lla-incompatible-dimensions
-
-   lla-efficiency-warning *lla-efficiency-warning-array-type*
-   lla-efficiency-warning-array-type *lla-efficiency-warning-array-conversion*
-   lla-efficiency-warning-array-conversion
-   
-;;; special-matrices
-   
-   wrapped-matrix elements make-matrix convert-matrix mref
-   lower-triangular-matrix upper-triangular-matrix hermitian-matrix
-   diagonal-matrix make-lower-triangular-matrix make-upper-triangular-matrix
-   make-hermitian-matrix make-diagonal dense lower hermitian upper vec diag
-   as-matrix
-   
-;;; printing
-   
-   *print-lla-precision* *pring-matrix-aligned* *print-matrix-padding*
-
-;;;; pinned-array - no exports
-
-;;;; linear-algebra
-   
-;;; factorizations
-
-   lu ipiv qr qr-r matrix-square-root xx left-square-root
-   right-square-root cholesky hermitian-factorization 
-   
-   spectral-factorization spectral-factorization-w spectral-factorization-z
-   
-   svd svd-u svd-d svd-vt
-   
-;;; fortran-call
-   
-   with-fp-traps-masked
-   
-;;; linear-algebra (note: some symbols already exported from factorizations)
-   
-   mm mmm outer solve invert least-squares invert-xx eigenvalues logdet det tr
-
-   ))
+   ;;package-init - no exports
+   ;;basics -- no exports
+   ;;types
+   #:lla-integer
+   #:lla-single
+   #:lla-double
+   #:lla-complex-single
+   #:lla-complex-double
+   ;;conditions
+   #:lla-internal-error
+   #:lla-unhandled-type
+   #:lapack-error
+   #:lapack-invalid-argument
+   #:lapack-failure
+   #:lapack-singular-matrix
+   #:lla-incompatible-dimensions
+   #:lla-efficiency-warning
+   #:*lla-efficiency-warning-array-type*
+   #:lla-efficiency-warning-array-type
+   #:*lla-efficiency-warning-array-conversion*
+   #:lla-efficiency-warning-array-conversion
+   ;;special-matrices
+   #:wrapped-matrix
+   #:elements
+   #:make-matrix
+   #:convert-matrix
+   #:mref
+   #:lower-triangular-matrix
+   #:upper-triangular-matrix
+   #:hermitian-matrix
+   #:diagonal-matrix
+   #:make-lower-triangular-matrix
+   #:make-upper-triangular-matrix
+   #:make-hermitian-matrix
+   #:make-diagonal
+   #:dense
+   #:lower
+   #:hermitian
+   #:upper
+   #:vec
+   #:diag
+   #:as-matrix
+   ;;printing
+   #:*print-lla-precision*
+   #:*pring-matrix-aligned*
+   #:*print-matrix-padding*
+   ;;pinned-array - no exports
+   ;;factorizations
+   #:lu
+   #:ipiv
+   #:qr
+   #:qr-r
+   #:matrix-square-root
+   #:xx
+   #:left-square-root
+   #:right-square-root
+   #:cholesky
+   #:hermitian-factorization
+   #:spectral-factorization
+   #:spectral-factorization-w
+   #:spectral-factorization-z
+   #:svd
+   #:svd-u
+   #:svd-d
+   #:svd-vt
+   ;;fortran-call
+   #:with-fp-traps-masked
+   ;;linear-algebra (note: some symbols already exported from factorizations)
+   #:mm
+   #:mmm
+   #:outer
+   #:solve
+   #:invert
+   #:least-squares
+   #:invert-xx
+   #:eigenvalues
+   #:logdet
+   #:det
+   #:tr))
