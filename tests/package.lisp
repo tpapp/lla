f7a76bf3d31f070a70093df2a87704ae5fcf7f5e
tpapp <tkpapp@gmail.com>
2012-03-31 12:08:05 +0200

    Fixed symbol conflict.

--- tests/package.lisp
+++ tests/package.lisp
@@ -2,4 +2,5 @@
   (:use #:cl #:iterate #:let-plus #:anaphora #:cl-num-utils #:cffi #:lift
         #:lla)
   (:import-from #:cl-num-utils-tests #:array=)
+  (:shadowing-import-from #:cl-num-utils #:sum) ; also in ITERATE
   (:export #:run))
