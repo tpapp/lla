c7f55261dd7475099a4a666fe00855152a559e0a
tpapp <tkpapp@gmail.com>
2013-03-25 12:26:06 +0100

    Cleaned up library definitions.
    Provide fallback for Linux, and default for unrecognized platform.
    Should fix https://github.com/tpapp/lla/issues/8

--- src/configuration.lisp
+++ src/configuration.lisp
@@ -9,13 +9,11 @@
 
 (defun default-libraries ()
   "Return a list of libraries.  The source conditions on the platform, relying TRIVIAL-FEATURES.  This function is only called when the libraries were not configured by the user, see the documentation on how to do that."
-  #+linux '("libblas.so.3gf" "liblapack.so.3gf")
+  #+linux '((:or "libblas.so.3gf" "libblas.so")
+            (:or "liblapack.so.3gf" "liblapack.so"))
   #+windows '("libblas.dll" "liblapack.dll")
   #+darwin '("libblas.dylib" "liblapack.dylib")
-  #-(or linux windows darwin)
-  (error "Could not figure out defaults for libraries on your platform. ~
-          Please read documentation for instructions on manual ~
-          configuration."))
+  #-(or linux windows darwin) '((:default "libblas") (:default "liblapack")))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (set-feature 'lla::int64 (query-configuration :int64 nil))
